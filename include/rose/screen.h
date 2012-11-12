48442d1ded165e542bc88030aac0147a58af470e
Rob Hoelz <rob@hoelz.ro>
2012-11-12 11:10:16 +0100

    Add integer writing operations

--- include/rose/screen.h
+++ include/rose/screen.h
@@ -3,5 +3,6 @@
 
 void screen_clear(void);
 void screen_write_string_at(const char *s, int x, int y);
+void screen_write_integer_at(long value, int radix, int x, int y);
 
 #endif
--- src/screen.c
+++ src/screen.c
@@ -6,6 +6,43 @@ static unsigned char *vram = (unsigned char *) 0xB8000;
 #define ROWS 20
 
 #define WIDTH 2 * COLS
+#define MAX_DIGITS 64
+
+static char char_map[] = "0123456789";
+
+static const char *
+itoa(long value, int radix)
+{
+    static char buffer[MAX_DIGITS + 2];
+    char *p;
+    int is_negative;
+
+    p = buffer + sizeof(buffer) - 1;
+
+    is_negative = value < 0;
+
+    if(is_negative) {
+        is_negative *= -1;
+    }
+
+    *p = '\0';
+    --p;
+
+    while(value >= radix) {
+        *p = char_map[value % radix];
+        --p;
+        value /= radix;
+    }
+
+    *p = char_map[value];
+
+    if(is_negative) {
+        --p;
+        *p = '-';
+    }
+
+    return p;
+}
 
 void
 screen_write_string_at(const char *s, int x, int y)
@@ -19,6 +56,16 @@ screen_write_string_at(const char *s, int x, int y)
         s++;
         vram_index += 2;
     }
+}
+
+void
+screen_write_integer_at(long value, int radix, int x, int y)
+{
+    const char *string_form;
+
+    string_form = itoa(value, radix);
+
+    screen_write_string_at(string_form, x, y);
 }
 
 void
