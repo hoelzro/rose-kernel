1fe5d5e916147ad2beb296f6d879084ea47f3c4b
Rob Hoelz <rob@hoelz.ro>
2012-11-12 14:36:43 +0100

    Initialize the GDT

--- src/kernel.c
+++ src/kernel.c
@@ -1,8 +1,11 @@
 #include <rose/screen.h>
+#include <rose/descriptor-tables.h>
 
 void
 kmain(void)
 {
     screen_clear();
+    gdt_init();
+
     screen_write_string_at("Hello from rOSe!", 0, 0);
 }
