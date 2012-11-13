8a404a9dee89d2760198205637e75d4ad9c1ab23
Rob Hoelz <rob@hoelz.ro>
2012-11-13 20:40:26 +0100

    Go to protected mode!

--- src/kernel.c
+++ src/kernel.c
@@ -1,11 +1,14 @@
 #include <rose/screen.h>
 #include <rose/descriptor-tables.h>
 
+extern void protected_mode_start(void);
+
 void
 kmain(void)
 {
     screen_clear();
     gdt_init();
+    protected_mode_start();
 
-    screen_write_string_at("Hello from rOSe!", 0, 0);
+    screen_write_string_at("Hello from rOSe (in protected mode!)", 0, 0);
 }
