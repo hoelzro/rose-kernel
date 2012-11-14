2afe79872279951e84dcfae97e81952cd528b70f
Rob Hoelz <rob@hoelz.ro>
2012-11-14 18:15:32 +0100

    Add capabilities_has_apic

--- include/rose/capabilities.h
+++ include/rose/capabilities.h
@@ -2,5 +2,6 @@
 #define ROSE_CAPABILITIES_H
 
 int capabilities_is_64bit(void);
+int capabilities_has_apic(void);
 
 #endif
--- src/capabilities.s
+++ src/capabilities.s
@@ -1,7 +1,13 @@
+FEATURE_FLAGS          EQU 0x01
 EXTENDED_FEATURE_FLAGS EQU 0x80000001
-LONGMODE_ENABLED       EQU 1<<29
+
+APIC_PRESENT     EQU 1<<9
+LONGMODE_ENABLED EQU 1<<29
 
 GLOBAL capabilities_is_64bit
+GLOBAL capabilities_has_apic
+
+; XXX verify that the CPU supports CPUID!
 
 capabilities_is_64bit:
     PUSH EBX
@@ -13,6 +19,24 @@ capabilities_is_64bit:
 
     MOV EAX, 0
     AND EDX, LONGMODE_ENABLED
+    SETNZ AL
+
+    POP EDX
+    POP ECX
+    POP EBX
+
+    RET
+
+capabilities_has_apic:
+    PUSH EBX
+    PUSH ECX
+    PUSH EDX
+
+    MOV EAX, FEATURE_FLAGS
+    CPUID
+
+    MOV EAX, 0
+    AND EDX, APIC_PRESENT
     SETNZ AL
 
     POP EDX
