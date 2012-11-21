02809db9e1cfe4f53e1af57b24b358f8cda7280f
Rob Hoelz <rob@hoelz.ro>
2012-11-21 23:20:06 +0100

    Add uint64_t

--- include/rose/stdint.h
+++ include/rose/stdint.h
@@ -1,9 +1,10 @@
 #ifndef ROSE_STDINT_H
 #define ROSE_STDINT_H
 
-typedef unsigned char  uint8_t;
-typedef unsigned short uint16_t;
-typedef unsigned int   uint32_t;
-typedef unsigned int   size_t;
+typedef unsigned char      uint8_t;
+typedef unsigned short     uint16_t;
+typedef unsigned int       uint32_t;
+typedef unsigned long long uint64_t;
+typedef unsigned int       size_t;
 
 #endif
