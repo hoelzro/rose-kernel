ebf8507310a2acdef23880bca28fda145b930583
Rob Hoelz <rob@hoelz.ro>
2013-10-01 21:11:41 +0200

    Rigorously test atomic functions with many threads

--- tests/verify-atomic.c
+++ tests/verify-atomic.c
@@ -1,6 +1,11 @@
 #include <assert.h>
+#include <errno.h>
+#include <stdio.h>
+#include <pthread.h>
 #include <rose/atomic.h>
 #include <rose/stdint.h>
+
+#define NUM_THREADS 4
 
 static void
 check_8bit_cas(void)
@@ -46,6 +51,41 @@ check_64bit_cas(void)
     assert(value == 18);
 }
 
+static void *
+many_thread_function_cas(void *udata)
+{
+    uint32_t *valuep = udata;
+    uint32_t value;
+    int i;
+
+    for(i = 0; i < 1000000; i++) {
+        value = *valuep;
+        while(!atomic_compare_and_set32(valuep, value, value + 1)) {
+            value = *valuep;
+        }
+    }
+    return NULL;
+}
+
+static void
+check_many_threads_cas(void)
+{
+    pthread_t threads[NUM_THREADS];
+    int i;
+    uint32_t value = 0;
+
+    for(i = 0; i < NUM_THREADS; i++) {
+        pthread_create(&threads[i], NULL, many_thread_function_cas, &value);
+    }
+
+    for(i = 0; i < NUM_THREADS; i++) {
+        void *result;
+        pthread_join(threads[i], &result);
+    }
+
+    assert(value == NUM_THREADS * 1000000);
+}
+
 int
 main(void)
 {
@@ -53,5 +93,6 @@ main(void)
     check_16bit_cas();
     check_32bit_cas();
     check_64bit_cas();
+    check_many_threads_cas();
     return 0;
 }
